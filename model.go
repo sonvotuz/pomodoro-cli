af31bfb03aa7857ff99b2ea748c8d17b8567637d
Son Vo <vnsonvo@gmail.com>
2024-04-14 00:10:19 +0700

    add timer to model

--- model.go
+++ model.go
@@ -4,11 +4,13 @@ import (
 	"time"
 
 	"github.com/charmbracelet/bubbles/progress"
+	"github.com/charmbracelet/bubbles/timer"
 )
 
 type model struct {
 	percent       float64
 	progress      progress.Model
+	timer         timer.Model
 	currentTimer  *time.Timer
 	timerDuration time.Duration
 	remainingTime time.Duration
