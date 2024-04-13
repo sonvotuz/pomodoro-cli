cd2ff94802b4b736f846f11b42649ee6fe591e3d
Son Vo <vnsonvo@gmail.com>
2024-04-14 01:24:11 +0700

    add bubble tick, remove timer, and update the Update method

--- bubble.go
+++ bubble.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/timer"
+	"github.com/charmbracelet/bubbles/progress"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -15,6 +15,8 @@ const (
 	padding  = 2
 	maxWidth = 80
 )
+
+type tickMsg time.Time
 
 var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
 
@@ -37,11 +39,12 @@ var keys = keyMap{
 }
 
 func initialModel() model {
-	return model{keys: keys, timerDuration: 25 * time.Minute}
+	return model{keys: keys, timerDuration: 25 * time.Minute,
+		progress: progress.New(progress.WithDefaultGradient())}
 }
 
 func (m model) Init() tea.Cmd {
-	return m.timer.Init()
+	return tickCmd()
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -50,12 +53,44 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch {
 		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
+		case key.Matches(msg, m.keys.Start):
+			if !m.inSession {
+				// TODO: s %m command
+				m.startTime = time.Now()
+				m.sessionType = "Work"
+				m.timerDuration = 1 * time.Minute
+				m.remainingTime = m.timerDuration
+				m.inSession = true
+			}
+			return m, tickCmd()
+
+		case key.Matches(msg, m.keys.Break):
+			if !m.inSession {
+				m.startTime = time.Now()
+				m.sessionType = "Break"
+				m.timerDuration = 5 * time.Minute
+				m.remainingTime = m.timerDuration
+				m.inSession = true
+			}
+			return m, tickCmd()
+		case key.Matches(msg, m.keys.List):
+			// TODO: printSessions func
+			return m, nil
+		case key.Matches(msg, m.keys.Stop):
+			if m.inSession {
+				m.inSession = false
+			}
+			return m, nil
 			// case key.Matches(msg, m.keymap.reset):
 			// 	m.timer.Timeout = timeout
 			// case key.Matches(msg, m.keymap.start, m.keymap.stop):
 			// 	return m, m.timer.Toggle()
+		default:
+			if !m.inSession {
+				showHelper()
+			}
+			return m, nil
 		}
-		return m, tea.Quit
 
 	case tea.WindowSizeMsg:
 		m.progress.Width = msg.Width - padding*2 - 4
@@ -64,17 +99,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 		return m, nil
 
-	case timer.TickMsg:
-		var cmds []tea.Cmd
-		var cmd tea.Cmd
+	case tickMsg:
+		fmt.Println("tick tick", m.remainingTime)
+		fmt.Println("duration", m.timerDuration)
+		m.remainingTime -= 1 * time.Second
 
-		m.remainingTime += m.timer.Interval
-		pct := m.remainingTime.Milliseconds() * 100 / m.timerDuration.Milliseconds()
-		cmds = append(cmds, m.progress.SetPercent(float64(pct)/100))
-
-		m.timer, cmd = m.timer.Update(msg)
-		cmds = append(cmds, cmd)
-		return m, tea.Batch(cmds...)
+		m.percent = 1 - float64(m.remainingTime.Milliseconds()-m.timerDuration.Milliseconds())/float64(m.timerDuration.Milliseconds())
+		fmt.Println("percent", m.percent)
+		return m, tickCmd()
+	case progress.FrameMsg:
+		fmt.Println("update")
+		progressModel, cmd := m.progress.Update(msg)
+		m.progress = progressModel.(progress.Model)
+		return m, cmd
 
 	default:
 		return m, nil
@@ -86,5 +123,12 @@ func (m model) View() string {
 		return showHelper()
 	}
 	pad := strings.Repeat(" ", padding)
+
 	return fmt.Sprintf("\n%s Timer: %s left\n", m.sessionType, m.remainingTime) + pad + m.progress.ViewAs(m.percent) + "\n\n" + pad + helpStyle(" - Press 'x' to stop\n - Press 'q' to quit")
+}
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Second, func(time.Time) tea.Msg {
+		return tickMsg{}
+	})
 }
