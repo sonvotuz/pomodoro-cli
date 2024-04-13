bfba22130d8c922a7ef16b3f8f4816e7374ef791
Son Vo <vnsonvo@gmail.com>
2024-04-13 23:46:45 +0700

    helper func to show commands

---
+++ utils.go
@@ -0,0 +1,5 @@
+package main
+
+func showHelper() string {
+	return "-------------------Usage-------------------\n\n - Press 's' to start work session.\n          s %m start work session for %m minutes\n - Press 'b' to take break.\n          b %m to take break for %m minutes\n - Press 'l' to list all completed today's sessions.\n - Press 'q' to quit.\n"
+}
