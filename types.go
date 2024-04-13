cdf1e35de996744668f998d07ee18bf7151f515c
Son Vo <vnsonvo@gmail.com>
2024-04-14 01:23:13 +0700

    remove timer and currenttimer from model struct

--- types.go
+++ types.go
@@ -5,14 +5,11 @@ import (
 
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/progress"
-	"github.com/charmbracelet/bubbles/timer"
 )
 
 type model struct {
 	percent       float64
 	progress      progress.Model
-	timer         timer.Model
-	currentTimer  *time.Timer
 	timerDuration time.Duration
 	remainingTime time.Duration
 	startTime     time.Time
